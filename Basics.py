3bbc2a5b7bff1c3f90fe6f9958081dde69630c59
Hrithwik <hrithwikssukumar@gmail.com>
2025-02-02 20:52:04 +0530

    updated basics

--- Basics.py
+++ Basics.py
@@ -8,3 +8,35 @@ mydataset = {
 myvar = pandas.DataFrame(mydataset)
 
 print(myvar)
+
+
+
+
+#Series
+import pandas as pd
+
+a = [1, 7, 2]
+
+myvar = pd.Series(a)
+
+print(myvar)
+
+
+import pandas as pd
+
+a = [1, 7, 2]
+
+myvar = pd.Series(a, index = ["x", "y", "z"])
+
+print(myvar)
+
+
+import pandas as pd
+
+calories = {"day1": 420, "day2": 380, "day3": 390}
+
+myvar = pd.Series(calories)
+
+print(myvar)
+
+
