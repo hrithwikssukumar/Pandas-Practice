ceed14a6eae58c28386b705c4a0cc6b55dc40e5e
Hrithwik <hrithwikssukumar@gmail.com>
2025-02-03 21:19:45 +0530

    added dataframe

--- Basics.py
+++ Basics.py
@@ -40,3 +40,32 @@ myvar = pd.Series(calories)
 print(myvar)
 
 
+
+#Dataframes
+
+import pandas as pd
+
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+
+#load data into a DataFrame object:
+df = pd.DataFrame(data)
+
+print(df)
+
+
+import pandas as pd
+
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+
+df = pd.DataFrame(data, index = ["day1", "day2", "day3"])
+
+print(df) 
+
+
+
